7a03a12a84744d0e9a206298e1adf445934123f0
Anson Miu <ansonmiu0214@gmail.com>
2017-07-11 21:34:56 +0800

    Created Square class with toString() for rank-file notation

---
+++ src/Square.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Anson on 11/7/2017.
+ */
+public class Square {
+
+  private final int row;
+  private final int col;
+  private Colour colour;
+
+  public Square(int row, int col, Colour colour) {
+    assert(colour != null && row >= 0 && col >= 0
+            && row < PawnRace.NUM_OF_ROWS && col < PawnRace.NUM_OF_ROWS);
+
+    this.row = row;
+    this.col = col;
+    this.colour = colour;
+  }
+
+  public Square(int row, int col) {
+    this(row, col, Colour.NONE);
+  }
+
+  public int getRow() {
+    return row;
+  }
+
+  public int getCol() {
+    return col;
+  }
+
+  public Colour occupiedBy() {
+    return colour;
+  }
+
+  public void setOccupier(Colour colour) {
+    assert(colour != null);
+    this.colour = colour;
+  }
+
+  @Override
+  public String toString() {
+    return Character.toString((char) (col + (int) 'a')) + (row + 1);
+  }
+
+}
