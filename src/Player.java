5aefd7a168b2e80ad1c1f58a4762774bb90c49a6
Anson Miu <ansonmiu0214@gmail.com>
2017-07-14 15:19:47 +0800

    Implemented makeMove() method in Player.java to distinguish between user and computer procedures

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
 /**
@@ -38,15 +41,65 @@ public class Player {
     return isComputerPlayer;
   }
 
-  public Set<Square> getPawns() {
-    return game.getPawns(colour);
+  public Set<Square> getPieces() {
+    return game.getPieces(colour);
   }
 
-  public Set<Move> getValidMoves() {
+  public List<Move> getValidMoves() {
     return game.getValidMoves(colour);
   }
 
+  private boolean isPassedPawn(Square square) {
+    assert(square != null && square.occupiedBy() == colour);
+
+    return game.isPassedPawn(square);
+  }
+
   public void makeMove() {
-    // TODO implement Player.makeMove()
+    Move move = null;
+    if (isComputerPlayer) {
+      // Advance passed pawn, if any
+      for (Move nextMove : getValidMoves()) {
+        if (isPassedPawn(nextMove.getFrom())) {
+          move = nextMove;
+          break;
+        }
+      }
+
+      // Find optimal move from MoveNode if no passed pawn
+      if (move == null) {
+        // Get optimal move
+        MoveNode tree = new MoveNode(colour);
+        List<Move> optimal = tree.getOptimal(game);
+        move = optimal.get(new Random().nextInt(optimal.size()));
+      }
+
+      System.out.println((colour == Colour.WHITE ? "WHITE " : "BLACK ") + "has played " + move.toString());
+      System.out.println();
+      System.out.println();
+
+    } else {
+      System.out.println();
+
+      // Setup input stream
+      Scanner in = new Scanner(System.in);
+      String san;
+      while (move == null) {
+        System.out.println("Enter your move in SAN (e.g. b5b6, b6xc5):");
+        san = in.nextLine();
+        move = game.parseMove(san);
+
+        if (move == null) {
+          System.out.println("Unable to parse move.");
+        } else if (!game.getValidMoves(colour).contains(move)) {
+          move = null;
+          System.out.println("Invalid move detected.");
+        }
+      }
+
+      System.out.println("You played " + move.toString());
+    }
+
+    game.applyMove(move);
   }
 }
